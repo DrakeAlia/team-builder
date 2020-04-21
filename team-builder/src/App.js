5695779b3abeda18e8b69c4559c3b0422a813110
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-04-21 16:49:39 -0700

    created team.js

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,25 +1,55 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
+import Team from './Team';
 
 function App() {
+  const [teamMember, setTeamMember ] = useState([{
+    id: 0,
+    name: 'Kylo Ren',
+    email: 'kyren@firstorder.com',
+    role: 'Intern',
+
+    id: 1,
+    name: 'Leon Kennedy',
+    email: 'leonsken@raccooncity.com',
+    role: 'Cop',
+
+    id: 2,
+    name: 'Geralt of Rivia',
+    email: 'grivia@whitewolf.com',
+    role: 'Monster Slayer',
+
+    id: 3,
+    name: 'Paul Atreides',
+    email: 'paulatr@dune.com',
+    role: 'Part-time Messiah',
+
+    id: 4,
+    name: 'Link',
+    email: 'mrfairy@hyrule.com',
+    role: 'Full-time Hero',
+
+  }]);
+
+    const addTeamMember = teammate => {
+      const newTeammate = {
+        id: Number.now(),
+        name: teammate.name,
+        email: teammate.email,
+        role: teammate.role,
+      };
+      setTeamMember ([...teammate, newTeammate]);
+    };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>My Team</h1>
+    <Form addTeamMember={addTeamMember}/>
+    <Team teammate={teammate}/>
+  </div>
   );
 }
 
---
+++ team-builder/src/Team.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Team = props => {
+  return (
+    <div className="Team">
+      {props.teammate.map(teammate => (
+        <div className="teammate" key={teammate.id}>
+          <h2>{teammate.name}</h2>
+          <p>{teammate.email}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
